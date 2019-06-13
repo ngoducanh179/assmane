a17fec1c553a17017bd4f81a967c0f323d5a1950
ngoducanh179 <46935024+ngoducanh179@users.noreply.github.com>
2019-06-13 16:06:54 +0700

    update index.php
    take

--- index.php
+++ index.php
@@ -4,24 +4,16 @@
  </head>
  <body>
  	<style type="text/css" media="screen">
- 		.anhthay{
-			width:20%;
-			position: relative;
-			bottom: 230px;
-			left:550px;
- 		}
+ 		
  		#Mot{
  			position: relative;
  			bottom: 140px;
  		}
  	</style>
- 	<h1>Hello Teacher! This is my Cloud App!</h1>
- 	<h2>My Name Is Thong</h2>
- 	<h2>Full Name: Nguyen Quang Thong</h2>
- 	<h2>ID: GCD17150</h2>
+ 	<h1>Tell me Who you are</h1>
+ 	<h2>Ngo Duc Anh</h2>
+ 	<h2>ID: GCD17028</h2>
  	<h2>Class: GCD0818</h2>
- 	<img class="anhthay" src="https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/10329285_1054169977931896_7440176948007827970_n.jpg?_nc_cat=110&_nc_oc=AQk5we59AQ7wEH79t6uJJqyY7eNZx4PHAVurJek4LhwrGajjMlU1rBRTzUwiOyWalaE&_nc_ht=scontent.fhan2-4.fna&oh=74a45d33cba6cb3fd1fcbe82efe40274&oe=5D81D519" alt="">
- 	<section id="Mot">
  	<br/>
      <a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
